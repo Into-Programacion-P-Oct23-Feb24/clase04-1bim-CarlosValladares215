2bfcaa715976e32824551893417e059e92680e24
Carlos Valladares <csvalladares2@utpl.edu.ec>
2023-11-01 16:37:57 -0500

    Promedios de tres notas

--- Ejemplo02/src/paquete1/Ejemplo2.java
+++ Ejemplo02/src/paquete1/Ejemplo2.java
@@ -31,6 +31,10 @@ public class Ejemplo2 {
         notaAsignatura1 = entrada.nextDouble();
         System.out.println("Ingrese nombre de asignatura 2");
         entrada.nextLine(); // limpieza de buffer
+        /* 
+           Se necesita hacer la limpieza de buffer cada que queramos ingresar 
+           un valor de tipo string por debajo de uno tipo numerico
+        */
         nombreAsignatura2 = entrada.nextLine();
         System.out.println("Ingrese nota de asignatura 2");
         notaAsignatura2 = entrada.nextDouble();
