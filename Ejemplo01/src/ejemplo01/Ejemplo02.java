39cba9c9469da039a239d4c1423bb5785357437b
Carlos Valladares <csvalladares2@utpl.edu.ec>
2023-11-01 15:36:35 -0500

    Print vs Println

--- Ejemplo01/src/ejemplo01/Ejemplo02.java
+++ Ejemplo01/src/ejemplo01/Ejemplo02.java
@@ -10,12 +10,13 @@ package ejemplo01;
  * @author reroes
  */
 public class Ejemplo02 {
+
     public static void main(String[] args) {
         // Ejemplo 
-        String nombreEstudiante = "René Rolando";
-        String apellidoEstudiante = "Elizalde Solano";
-                
-        System.out.println(nombreEstudiante+apellidoEstudiante);
-        System.out.println(nombreEstudiante+" "+apellidoEstudiante);
+        String nombreEstudiante = "Carlos Sebastian";
+        String apellidoEstudiante = "Valladares Chunata";
+
+        System.out.println(nombreEstudiante + apellidoEstudiante);
+        System.out.println(nombreEstudiante + " " + apellidoEstudiante);
     }
 }
--- Ejemplo01/src/ejemplo01/Ejemplo04.java
+++ Ejemplo01/src/ejemplo01/Ejemplo04.java
@@ -11,12 +11,17 @@ package ejemplo01;
  */
 public class Ejemplo04 {
     public static void main(String[] args) {
+        /*
+        Println vs print
+        Println da un salto de lineaen la pantalla, mientras que print no da saltos y lo deja todo en la misma linea de la pantalla
+        */
         String nombreEstudiante = "René Rolando";
         String apellidoEstudiante = "Elizalde Solano";
         int nacimiento = 1983;
-        System.out.print(nombreEstudiante + " ");
+        System.out.println(nombreEstudiante + " ");
         System.out.print(apellidoEstudiante + " ");
         System.out.print(nacimiento + " ");
         System.out.print(nombreEstudiante + " ");
+        
     }
 }
